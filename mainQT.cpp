52e17084c03b7f85618c7db5ed80e75e8d997629
Алексей Ершков <ershkov.alex2000@gmail.com>
2019-12-08 17:20:11 +0300

    QT build added

--- mainQT.cpp
+++ mainQT.cpp
@@ -1,22 +1,6 @@
-//#include "View.h"
-//#include <cstring>
 //
+// Created by Алексей on 08/12/2019.
 //
-//
-//int main(int argc, char** argv) {
-//    View *view;
-//    if (argc > 1){
-//        view = new View(true);
-//    } else {
-//        view = new View;
-//    }
-//    auto screen = update_action::DEFAULT;
-//    while (screen != update_action::CLOSE){
-//        screen = view->update(screen, std::cin, std::cout);
-//    }
-//    view->system_clear();
-//    return 0;
-//}
 
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QWidget>
